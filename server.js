24f5ea69c06db579783dee6b1e67f79754233bd9
HenniePenny <h.kressner@hotmail.de>
2022-08-21 11:27:02 +0200

    add get /api/notes route

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const notes = require("./db/db.json");
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+//serving the static resources (where to take the static resources is)
 app.use(express.static("public"));
 
-app.listen(PORT, () => console.log(""));
+// app.listen(PORT, () => console.log(""));
+
+//whenever I navigate localhost/3001/notes
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+
+//read the db.json file and return all saved notes as JSON
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+//all other routes should return the index.html file
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}.`));
